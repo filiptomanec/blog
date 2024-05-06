2abac4cc5d8391556a1bea671111fce7dff0de9e
Filip Tomanec <filip.tomanec@artio.cz>
2024-05-06 10:38:45 +0200

    Add basic test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders home page link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElements = screen.getAllByText(/Travel Blog/i);
+  expect(linkElements[0]).toBeInTheDocument();
 });
